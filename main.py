1d6d7f46e4bc34e3e0ea6b9e746d5664acafe33f
aztec-dev <aztech.dev1@gmail.com>
2025-02-12 23:07:38 +1000

    Added working list view.

--- main.py
+++ main.py
@@ -4,18 +4,48 @@ Date: 10/02/2025
 Author: Azariah Pundari (aztec-dev)
 """
 import json
+import os
+
+FILE_NAME = 'tasks.json'
 
 tasks = {
-    "Id": "1",
-    "Task": "sample task",
+    "id": "1",
+    "description": "sample task",
+    "status" : "status",
+    "createdAt": "created at",
+    "updatedAt": "updated at"
 }
 
 tasks_to_json = json.dumps(tasks, indent=4)
+MENU = "add [task name/description] => adds a new task\n\
+update [ID] => updates a task based on ID\n\
+delete [ID] => deletes a task based on ID\n\
+list [status] => list all tasks based on status\n"
+
 
 def main():
-    """Main program"""
-    with open("tasks.json", "a") as task_file:
-        # add to file
-        task_file.write(tasks_to_json)
+    """Main program."""
+    print(MENU)
+
+    option = input("task-cli ")
+    task_name = ""
+
+    while option != "exit":
+        if option == "list":
+            print(open_tasks(FILE_NAME))
+        elif option == f"add {task_name}":
+            while task_name == "":
+                print("Incorrect task name")
+        option = input("task-cli ")
+    print("exit")
+
+def open_tasks(file_name):
+    """Displays a list of the current tasks within the file."""
+    # Check if file exists, if not => opens a readable file.
+    if not os.path.exists(file_name):
+        with open(file_name, "r") as task_file:
+            # Display data
+            data = json.load(task_file)
+            return data
 
 main()
