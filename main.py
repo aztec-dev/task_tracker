0a2ead5a92877f16678d04189592e78f731e6454
aztec-dev <aztech.dev1@gmail.com>
2025-03-19 15:17:12 +1000

    Fixed overused status & data function calls.

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ style = Style.from_dict({
 })
 
 MENU = "\033[33madd [task name/description]\033[0m => adds a new task\n\
-\033[33mupdate [ID]\033[0m => updates a task based on ID\n\
+\033[33mupdate [ID] [task name/description]\033[0m => updates a task based on ID\n\
 \033[33mdelete [ID]\033[0m => deletes a task based on ID\n\
+\033[33mmark-[condition]\033[0m => mark a task as done, todo, in-progress\n\
 \033[33mlist [status]\033[0m => list all tasks based on status\n"
 
 
@@ -31,7 +32,8 @@ def main():
     Behold, the program!
     """
     print(MENU)
-
+    status = validate_file(FILE_NAME)
+    data = read_data(FILE_NAME)
     # option = input('task-cli ')
     option = prompt(
         [('class:propmt', 'task-cli '), ('class:input', '')],
@@ -40,45 +42,50 @@ def main():
 
     while option != "exit":
         if option.startswith("list"):
-            status = option[5:]  # Extract status
-            list_tasks(FILE_NAME, status)
+            condition = option[5:]  # Extract condition
+            list_tasks(condition, status, data)
         elif option.startswith("add "):
             task_name = option[4:]  # Extract task name
-            add_task(FILE_NAME, task_name)
-        elif option.startswith("update "):
+            add_task(FILE_NAME, task_name, status, data)
+        elif option.startswith("update"):
             task_id = option[7:8]  # Extract task ID
             updated_task = option[9:]  # Extract updated task name
-            # print(len(task_id))
-            # print(len(updated_task))
-            if task_id == "" or len(task_id) == 0 and len(updated_task) == 0:
-                print("Task ID is empty.")
-            elif len(task_id) == 0:
-                print("No task ID inputed. Please add task ID.")
+            if task_id == "":  # Handle empty ID case.
+                print("\033[1;31;48mNo valid task ID defined. Try again.\033[0")
+            elif task_id == "0":  # Handle 0 index ID case.
+                print("\033[1;31;48mID can't be 0. Try again.\033[0")
+            elif len(updated_task) == 0:  # Handle empty task name/description case
+                print("\033[1;31;48mNo new task name/description defined. Try again.\033[0m")
             else:
-                update_task(FILE_NAME, task_id, updated_task)
-        elif option.startswith("delete "):
+                update_task(FILE_NAME, task_id, updated_task, status, data)
+        elif option.startswith("delete"):
             task_id = option[7:]
-            delete_task(FILE_NAME, task_id)
+            delete_task(FILE_NAME, task_id, status, data)
+        elif option.startswith("mark-"):
+            condition = option[5:]
+            mark_task(condition)
+        else:
+            print("\033[1;31;48mIncorrect command. Try again.\033")
         option = prompt(
         [('class:propmt', 'task-cli '), ('class:input', '')],
         style=style
         )
     print("exit")
 
-def add_task(file_name:str, task_name:str):
+def add_task(file_name:str, task_name:str, status:bool, data:list):
     """
     Adds a task to a file based on the users input.
     
     Parameters:
     file_name (str): Name of the file to write data to.
     task_name (str): Name of the task.
+    status (bool): Bool value defining the state of the file.
+    data (list): Represents a list of dictionary objects that represents a task.
 
     Returns:
     NA
     Displays a status message.
     """
-    status = validate_file(file_name)
-    data = read_data(file_name)
 
     # Data processing
     created_date = datetime.now().strftime("%Y-%m-%d %H:%M")
@@ -95,11 +102,9 @@ def add_task(file_name:str, task_name:str):
     if status:
         with open(file_name, 'w') as file:
             json.dump(data, file, indent=4)
-    else:
-        pass
     print(f"Task added successfully (ID: {str(id_generator)})")
 
-def update_task(file_name:str, task_id:str, updated_task:str):
+def update_task(file_name:str, task_id:str, updated_task:str, status:bool, data:list):
     """
     Updates a task based on the task ID provided by the user.
 
@@ -107,13 +112,13 @@ def update_task(file_name:str, task_id:str, updated_task:str):
     file_name (string): Represents the file path
     task_id (str): Represents the task ID
     updated_task (str): Represents the updated task that the user inputs
+    status (bool): Bool value defining the state of the file.
+    data (list): Represents a list of dictionary objects that represents a task.
 
     Returns:
     NA
     Displays a status message.
     """
-    status = validate_file(file_name)
-    data = read_data(file_name)
 
     # Data processing
     updated_date = datetime.now().strftime("%Y-%m-%d %H:%M")
@@ -129,19 +134,19 @@ def update_task(file_name:str, task_id:str, updated_task:str):
             json.dump(data, file, indent=4)
     print(f"Task {task_id} updated successfully.")
 
-def delete_task(file_name:str, task_id:str):
+def delete_task(file_name:str, task_id:str, status:bool, data:list):
     """
     Deletes a task from the tasks.json file based on the ID given.
 
     Parameters:
     file_name (str): Name of the file to overwrite data.
     task_id (str): Represents the task ID
+    status (bool): Bool value defining the state of the file.
+    data (list): Represents a list of dictionary objects that represents a task.
 
     Returns:
     NA
     """
-    status = validate_file(file_name)
-    data = read_data(file_name)
 
     for i in range(len(data)):
         if data[i]["id"] == int(task_id):
@@ -149,24 +154,23 @@ def delete_task(file_name:str, task_id:str):
             break
 
     if status:
-        # data[0]["id"] = 
         with open(file_name, 'w') as file:
             json.dump(data, file, indent=4)
     
     print(f"Task {task_id} deleted successfully.")
 
-def list_tasks(file_name:str, task_status:str):
+def list_tasks(task_status:str, status:bool, data:list):
     """
     Reads from the file and lists the tasks.
     
     Parameters:
     file_name (str): Name of the file to read/display data from.
+    status (bool): Bool value defining the state of the file.
+    data (list): Represents a list of dictionary objects that represents a task.
 
     Returns:
     prints a list of tasks and filters them based on status.
     """
-    status = validate_file(file_name)
-    data = read_data(file_name)
 
     if status and len(data) != 0:
         if task_status == "":
@@ -213,6 +217,14 @@ def validate_file(file_name:str):
     else:
         create_file(file_name, tasks)
         return True
+
+def mark_task(file_name:str, condition:str):
+    """
+    Marks a task as in-progress or done.
+
+    Paramteres:
+    file_name (str): Name of the file containing 
+    """
     
 def read_data(file_name:str):
     """
