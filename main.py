071408ae5d62399928886d0cd68bc65ba8b7d1db
aztec-dev <aztech.dev1@gmail.com>
2025-03-19 13:47:53 +1000

    Added sorting task by condition

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Date: 10/02/2025
 """
 import json
 import os
+from prompt_toolkit import prompt
+from prompt_toolkit.styles import Style
 from datetime import datetime
 from custom_id_generator import CustomIdGenerator
 FILE_NAME = 'tasks.json'
@@ -12,10 +14,16 @@ FILE_NAME = 'tasks.json'
 tasks = []
 id_generator = CustomIdGenerator()
 
-MENU = "add [task name/description] => adds a new task\n\
-update [ID] => updates a task based on ID\n\
-delete [ID] => deletes a task based on ID\n\
-list [status] => list all tasks based on status\n"
+# cli style and input style
+style = Style.from_dict({
+    'prompt': 'bg:green fg:white', 
+    '': 'fg:yellow'
+})
+
+MENU = "\033[33madd [task name/description]\033[0m => adds a new task\n\
+\033[33mupdate [ID]\033[0m => updates a task based on ID\n\
+\033[33mdelete [ID]\033[0m => deletes a task based on ID\n\
+\033[33mlist [status]\033[0m => list all tasks based on status\n"
 
 
 def main():
@@ -24,22 +32,37 @@ def main():
     """
     print(MENU)
 
-    option = input("task-cli ")
+    # option = input('task-cli ')
+    option = prompt(
+        [('class:propmt', 'task-cli '), ('class:input', '')],
+        style=style,
+    )
 
     while option != "exit":
-        if option == "list":
-            list_tasks(FILE_NAME)
+        if option.startswith("list"):
+            status = option[5:]  # Extract status
+            list_tasks(FILE_NAME, status)
         elif option.startswith("add "):
             task_name = option[4:]  # Extract task name
             add_task(FILE_NAME, task_name)
         elif option.startswith("update "):
             task_id = option[7:8]  # Extract task ID
-            updated_task = option[9:]
-            update_task(FILE_NAME, task_id, updated_task)
+            updated_task = option[9:]  # Extract updated task name
+            # print(len(task_id))
+            # print(len(updated_task))
+            if task_id == "" or len(task_id) == 0 and len(updated_task) == 0:
+                print("Task ID is empty.")
+            elif len(task_id) == 0:
+                print("No task ID inputed. Please add task ID.")
+            else:
+                update_task(FILE_NAME, task_id, updated_task)
         elif option.startswith("delete "):
             task_id = option[7:]
             delete_task(FILE_NAME, task_id)
-        option = input("task-cli ")
+        option = prompt(
+        [('class:propmt', 'task-cli '), ('class:input', '')],
+        style=style
+        )
     print("exit")
 
 def add_task(file_name:str, task_name:str):
@@ -60,13 +83,14 @@ def add_task(file_name:str, task_name:str):
     # Data processing
     created_date = datetime.now().strftime("%Y-%m-%d %H:%M")
     stripped_task = task_name.strip('"')
+    task_status = "todo"
 
     if len(data) > 0:
         sequential_id = id_generator.generate_task_id(data[len(data) - 1]["id"])
     else:
         # Handle case when data is empty
         sequential_id = id_generator.generate_task_id(0)
-    data.append({"id": sequential_id, "task": stripped_task, "createdAt": created_date})
+    data.append({"id": sequential_id, "task": stripped_task, "createdAt": created_date, "status": task_status})
 
     if status:
         with open(file_name, 'w') as file:
@@ -131,7 +155,7 @@ def delete_task(file_name:str, task_id:str):
     
     print(f"Task {task_id} deleted successfully.")
 
-def list_tasks(file_name:str):
+def list_tasks(file_name:str, task_status:str):
     """
     Reads from the file and lists the tasks.
     
@@ -139,21 +163,44 @@ def list_tasks(file_name:str):
     file_name (str): Name of the file to read/display data from.
 
     Returns:
-    prints a string.
+    prints a list of tasks and filters them based on status.
     """
     status = validate_file(file_name)
     data = read_data(file_name)
 
     if status and len(data) != 0:
-        for task in data:
-            print(f'{task["id"]}: {task["task"]}')
+        if task_status == "":
+            for task in data:
+                print(f'{task["id"]}: {task["task"]} {task["status"]}')
+        elif task_status == "done":
+            sort_tasks(data, "done")
+        elif task_status == "todo":
+            sort_tasks(data, "todo")
+        elif task_status == "in-progress":
+            sort_tasks(data, "in-progress")
     else:
         print("You haven't added any tasks yet.")
+
+def sort_tasks(tasks:dict, condition:str):
+    """
+    Sorts the list of tasks depending on the condtion:
+    --- todo
+    --- in progress
+    --- done
+
+    parameters:
+    tasks (dict): The dictionary of tasks that will be sorted
+    condition (str): The condition to sort by
+    """
+    sorted_tasks = sorted(tasks, key=lambda item: item["status"] != condition)
+    for task in sorted_tasks:
+        print(f'{task["id"]}: {task["task"]} {task["status"]}')
 
 def validate_file(file_name:str):
     """
     Validates the files existence and validates the data in the file.
     Creates file if it doesn't exists.
+
     Parameters:
     file_name (str): Name of the file to be validated.
 
